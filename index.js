0f150fa7c4b09bb024da98b20358c2daf201edaa
Dalton Walker <daltonwalkerdw@yahoo.com>
2020-02-13 11:53:43 -0500

    bugs

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 // implement your API here
+const express = require("express")
+
+const db = require("./data/db.js")
+
+const port = 7101
+const host = "127.0.0.1"
+const server = express()
+
+//  middleware that allows express to parse json request
+server.use(express.json())
+
+
+server.get("/", (req, res) => {
+  res.json({message: "Hello World!"})
+})
+
+// server.get("/", (req, res) => {
+    
+// })
+
+server.get("/api/users", (req, res) => {
+  db.find()
+  .then(res => {
+    res.json(res)
+  })
+  .catch(err => {
+    res.status(500).send( {message: "Not Working"} )
+  })
+  
+})
+
+// server.get("/api/users/:id", (req, res) => {
+
+// })
+
+server.listen(port, host , () => {
+    console.log(`server started at port ${port}`)
+})
